7924ea75909b2e7667e97490b09a3dcf1c7e91bb
Cangyuan Li <everest229@gmail.com>
2021-10-08 11:59:11 -0400

    first working run

--- src/clean.py
+++ src/clean.py
@@ -8,13 +8,13 @@ import bibtexparser
 
 base_path = Path(__file__).parent.parent
 
-articles = [
+articles = {
     "a",
     "an",
     "the"
-]
+}
 
-conjunctions = [
+conjunctions = {
     "and",
     "but",
     "for",
@@ -22,9 +22,9 @@ conjunctions = [
     "or",
     "so",
     "yet"
-]
+}
 
-prepositions = [
+prepositions = {
     "abaft",
     "aboard",
     "about",
@@ -131,7 +131,83 @@ prepositions = [
     "within",
     "without",
     "worth"
-]
+}
+
+acronyms = {
+    "adtv",
+    "amex",
+    "apr",
+    "arm",
+    "bea",
+    "cagr",
+    "cao",
+    "capex",
+    "cb",
+    "cd",
+    "cfa",
+    "cfm",
+    "cfo",
+    "cia",
+    "cisa",
+    "cma",
+    "cmo",
+    "cmp",
+    "cob",
+    "coo",
+    "cpa",
+    "cpp",
+    "cso",
+    "cto",
+    "djia",
+    "eft",
+    "eps",
+    "etf",
+    "fdic",
+    "forex",
+    "frb",
+    "gdp",
+    "gmp",
+    "gnp",
+    "ipo",
+    "ira",
+    "llc",
+    "loi",
+    "mmkt",
+    "mtd",
+    "nasdaq",
+    "nav",
+    "ncnd",
+    "nda",
+    "neer",
+    "nyse",
+    "p&l",
+    "p/e",
+    "pe",
+    "pfd",
+    "ppp",
+    "ppplf",
+    "psp",
+    "qtd",
+    "qte",
+    "rbi",
+    "reit",
+    "roa",
+    "roce",
+    "roe",
+    "roi",
+    "roic",
+    "rona",
+    "ros",
+    "sba",
+    "sec",
+    "siv",
+    "tsa",
+    "tsr",
+    "usa",
+    "wc",
+    "ytd",
+    "ytm"
+}
 
 # functions
 
@@ -153,9 +229,52 @@ def is_preposition(word: str) -> bool:
     else:
         return False
 
+def is_acronym(word: str) -> bool:
+    if word in acronyms or "." in word:
+        return True
+    else:
+        return False
+
+def clean_and_split(field: str) -> list:
+    field = field.strip() # strip whitespace off ends
+    field = " ".join(field.split()) # normalize whitespace to one
+    field = field.lower()
+
+    word_list = field.split(" ")
+
+    return word_list
+
 with open(base_path / "test_files/cl_sg_bib.bib", encoding="utf8") as bib:
     bib_db = bibtexparser.load(bib)
 
+for entry in bib_db.entries:
+    try:
+        title = entry["title"]
+    except KeyError:
+        entry["title"] = None
+        continue
+    
+    word_list = clean_and_split(title)
 
+    corrected_list = []
+    for idx, word in enumerate(word_list):
+        if is_article(word) or is_conjuction(word) or is_preposition(word):
+            correct_word = word.lower()
+        elif is_acronym(word):
+            correct_word = word.upper()
+        else:
+            correct_word = word.title()
 
+        if idx in {0, len(word_list)} or ":" in word_list[idx - 1]:
+            correct_word = word.title()
+        if "-" in word:
+            dash_pos = word.index("-")
+            correct_word = word[:dash_pos + 1] + word[dash_pos + 1].lower() + word[dash_pos + 2:]
+        
+        corrected_list.append(correct_word)
+        
+    correct_entry = " ".join(corrected_list)
+    entry.update({"title": correct_entry})
 
+with open(base_path / "cl_sg_corrected.bib", "w", encoding="utf8") as f:
+    bibtexparser.dump(bib_db, f)
